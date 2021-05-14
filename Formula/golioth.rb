c245ca91a71847920a5737af4a6c9cc358675109
goreleaserbot <goreleaser@golioth.io>
2021-05-14 10:51:23 -0400

    Brew formula update for golioth version v0.12.0

--- Formula/golioth.rb
+++ Formula/golioth.rb
@@ -5,21 +5,21 @@
 class Golioth < Formula
   desc "Tool to help interact with Golioth Platform and with IoT development in general."
   homepage "https://docs.golioth.dev/"
-  version "0.11.0"
+  version "0.12.0"
   license "Apache-2.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://storage.googleapis.com/golioth-cli-releases/golioth/v0.11.0/golioth_0.11.0_macos_64bit.tar.gz", :using => CurlDownloadStrategy
-    sha256 "496d1d51b5ff09268a3ea148f159bb17e921a97ecca824bdf2df6f156213b3b6"
+    url "https://storage.googleapis.com/golioth-cli-releases/golioth/v0.12.0/golioth_0.12.0_macos_64bit.tar.gz", :using => CurlDownloadStrategy
+    sha256 "ebfdb5bd85cc203272b36f7a309bf0ce41307e5976b0f06f9a1fcaaa1e8497df"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://storage.googleapis.com/golioth-cli-releases/golioth/v0.11.0/golioth_0.11.0_linux_64bit.tar.gz", :using => CurlDownloadStrategy
-    sha256 "c28f70d44623ba964366a369a0f89d50d8700ce746fa18bd9a8e4975ddafea22"
+    url "https://storage.googleapis.com/golioth-cli-releases/golioth/v0.12.0/golioth_0.12.0_linux_64bit.tar.gz", :using => CurlDownloadStrategy
+    sha256 "938094281ad6be4d834a0cdb2e9d0aaf672bdf88f6b20e40e65edbab7be5a236"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://storage.googleapis.com/golioth-cli-releases/golioth/v0.11.0/golioth_0.11.0_linux_arm64.tar.gz", :using => CurlDownloadStrategy
-    sha256 "e70f1e6bf97cfea7b7d3fa478e474e425905af5846b3f8da5d03e04535d0c064"
+    url "https://storage.googleapis.com/golioth-cli-releases/golioth/v0.12.0/golioth_0.12.0_linux_arm64.tar.gz", :using => CurlDownloadStrategy
+    sha256 "cace9ef647dab70f7c10dc89e87a09ee3c1c89875360431775466266e0b75eb9"
   end
 
   def install
