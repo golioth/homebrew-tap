c31063b425e9c9cc0c6b86472658590846534297
goreleaserbot <goreleaser@golioth.io>
2021-06-10 14:13:48 -0400

    Brew formula update for goliothctl version v1.0.2

--- Formula/goliothctl.rb
+++ Formula/goliothctl.rb
@@ -5,25 +5,25 @@
 class Goliothctl < Formula
   desc "Tool to help interact with Golioth Platform and with IoT development in general."
   homepage "https://docs.golioth.dev/"
-  version "1.0.0"
+  version "1.0.2"
   license "Apache-2.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://storage.googleapis.com/golioth-cli-releases/goliothctl/v1.0.0/goliothctl_1.0.0_macos_64bit.tar.gz", :using => CurlDownloadStrategy
-      sha256 "ae9c4c47b0bfa165d2cd93d55129c0b749b699884918e4c1f26c24c54fb3832a"
+      url "https://storage.googleapis.com/golioth-cli-releases/goliothctl/v1.0.2/goliothctl_1.0.2_macos_64bit.tar.gz", :using => CurlDownloadStrategy
+      sha256 "60b03eab31333df501e3786226c8a98844571e7f38f6856ae62fd9e853fdab66"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://storage.googleapis.com/golioth-cli-releases/goliothctl/v1.0.0/goliothctl_1.0.0_linux_64bit.tar.gz", :using => CurlDownloadStrategy
-      sha256 "15b4029a91ce3b519b5a463fbfecdefde11289ddfffce4b9a63b08b28dcda77a"
+      url "https://storage.googleapis.com/golioth-cli-releases/goliothctl/v1.0.2/goliothctl_1.0.2_linux_64bit.tar.gz", :using => CurlDownloadStrategy
+      sha256 "5550aa6a6159c80288c5d5d26a456838f37d0ef988f14a96eaffaabbb3a53a0d"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://storage.googleapis.com/golioth-cli-releases/goliothctl/v1.0.0/goliothctl_1.0.0_linux_arm64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "49c2ce779bd2238b5ad9fbc99d6d4547db7a94bf5a14275e247d0168b5118ea1"
+      url "https://storage.googleapis.com/golioth-cli-releases/goliothctl/v1.0.2/goliothctl_1.0.2_linux_arm64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "b8e098e989834999946cf57cf4bf9c66b33a2240d815a35ca66c56b9013c5cbf"
     end
   end
 
